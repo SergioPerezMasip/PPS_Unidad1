29d119525c8e5a06338e365105202cbfb65ac166
SergioPerezMasip <148375562+SergioPerezMasip@users.noreply.github.com>
2023-11-27 15:00:35 +0100

    Add files via upload
    Ejercicios 1,2 y 3 de la Unidad 1 de PPS relacionados con Python

---
+++ binario.py
@@ -0,0 +1,28 @@
+def binarioaDecimal(strbinario):
+    numero_decimal = 0
+    
+
+    for posicion, digito_string in enumerate(strbinario[::-1]):
+        numero_decimal += int(digito_string) * 2 ** posicion
+
+    print(f"El número binario {strbinario} es el {numero_decimal} en decimal")
+    return numero_decimal
+
+
+def esBinario(strbinario):
+#Devuelve True o False si la cadena de caracteres (strbinario) que se ha pasado como parámetro contiene una cadena binaria
+    
+    for digitos in strbinario:
+        if ((digitos == "0") or (digitos == "1")):
+            print("El numero es binario")
+            
+
+            binarioaDecimal(strbinario)
+            return True
+
+        else:
+            print("El número no es binario")
+            return False
+
+numero = input("Introduce un número binario: ")
+esBinario(numero)
---
+++ lista.py
@@ -0,0 +1,40 @@
+def estaEnRango(valor, minimo, maximo):
+#Devuelve True o False determinando qué valor se encuentra entre el mínimo y el máximo
+
+    if valor > maximo:
+        print("El número es mayor al intervalo indicado. Por favor, introduzca otra vez el valor")
+        return False
+    elif valor < minimo:
+        print("El número es menor al intervalo indicado. Por favor, introduzca otra vez el valor")
+        return False
+    else:
+        print("El número introducido se encuentra dentro del intervalo")
+        return True
+     
+
+def estaEnLista(valor,lista):
+#Devuelve True or False determinando si el valor está en la lista
+
+    encontrado = 0
+
+    for numeros in lista:
+        if valor == numeros:
+            encontrado = 1
+    
+    if encontrado == 1:
+        print("El número se encuentra dentro de la lista")
+        return True
+    else:
+        print("El número está fuera de la lista")
+        return False
+
+
+valor = int(input("Introduzca un número comprendido entre los dos límites: "))
+minimo = 1
+maximo = 20
+
+estaEnRango(valor,minimo,maximo)
+lista = [5, 14, 11, 3, 2, 1, 15, 19]
+
+valor = int(input("Introduzca un número: "))
+estaEnLista(valor,lista)
